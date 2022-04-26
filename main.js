3677cf3d8fc404c524ba7c5a9a5c81f0efa15a26
Sushi <98758109+Sushi314@users.noreply.github.com>
2022-04-26 19:21:58 -0400

    Has the basic HTML with some info

--- main.js
+++ main.js
@@ -2,18 +2,36 @@ import './assets/css/style.css'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import {DepthFormat} from 'three';
+import { ColorKeyframeTrack } from 'three';
 
 //Builds canvas
 const scene = new THREE.Scene();
 const scene1 = new THREE.Scene();
 const aspectRatio = window.innerWidth / window.innerHeight;
-const camera = new THREE.PerspectiveCamera(90, aspectRatio, .001, 3000);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
-renderer.setPixelRatio(window.devicePixelRatio);
 
-camera.position.set(150, 200, 1000);
+//Camera
+const camera = new THREE.PerspectiveCamera(80, aspectRatio, .001, 30000);
+const cameraPositions = {
+  x: -600,
+  y: -2200,
+  z: 600,
+};
+
+//Move camera and objects on scroll
+function moveCamera() {
+  const x = Object.values(cameraPositions)[0] + document.body.getBoundingClientRect().top;
+  const y = Object.values(cameraPositions)[1] + document.body.getBoundingClientRect().top;
+  const z = Object.values(cameraPositions)[2] + document.body.getBoundingClientRect().top;
+  //Move camera postion
+    camera.position.x = x * -1.8;
+    camera.position.y = y * -0.5;
+    camera.position.z = z * 1;
+ };
+document.body.onscroll = moveCamera;
+moveCamera();
 
 //Background
 const space = new THREE.TextureLoader().load('./assets/images/Stars.jpg');
@@ -62,7 +80,7 @@ moon.position.set(100, 0, 0);
 
 
 //Lighting
-const pointLight = new THREE.PointLight(0xffffff, 2, 5000);
+const pointLight = new THREE.PointLight(0xffffff, 10, 1500, 2);
 const ambientLight = new THREE.AmbientLight(0xffffff, .001);
 const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(500, 50);
@@ -71,22 +89,23 @@ pointLight.position.set(0, 0, 0);
 
 //Add Stars(Array(Number of stars))
 function addStar() {
-  const geometry = new THREE.SphereGeometry(1, 50, 50);
-  const material = new THREE.MeshStandardMaterial({
+  const geometry = new THREE.SphereGeometry(2, 50, 50);
+  const material = new THREE.MeshBasicMaterial({
     color: 0xfcf4bb,
-    normalMap: normalTexture
   });
   const star = new THREE.Mesh(geometry, material);
   //FloatSpread (how large the star field will be)
-  const [x, y, z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(2000));
+  const [x, y, z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(5000));
   star.position.set(x, y, z);
   scene.add(star);
 };
-Array(1000).fill().forEach(addStar);
+Array(2000).fill().forEach(addStar);
 
 //Add the objects to scene
 
-scene.add(ambientLight, pointLight, earthObj, sun, /*lightHelper,  gridHelper*/);
+scene.add(pointLight, earthObj, sun,  /*ambientLight, lightHelper*/);
+
+
 
 
 //animates scene
@@ -94,12 +113,13 @@ function animate() {
   requestAnimationFrame(animate);
   sun.rotation.y += -0.01;
 
-  moon.rotation.y += 0.5; //Moon spin
-  moonObj.rotation.y += 0.08; //Moon speed around Earth
+  moon.rotation.y += 0.00001; //Moon spin
+  moonObj.rotation.y += 0.04; //Moon speed around Earth
 
-  earth.rotation.y += 0.01; //Earth spin
+  earth.rotation.y += 0.03; //Earth spin ALSO CONTROLS moonObj!!!
   earthObj.rotation.y += 0.008; //Earth speed around Sun
-  
+
+  renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   controls.update();
   renderer.render(scene, camera);
