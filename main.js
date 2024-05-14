a159803c4194b96c0f0299ca2e62f45b1cbb4f9c
Sushi <mridea314@gmail.com>
2024-05-13 20:24:00 -0400

    Updated main.js to scale the scene with the window size.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './assets/css/index.css';
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import starsUrl from './assets/images/space.jpg';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 import sunUrl from './assets/images/sun.jpg';
 import earthUrl from './assets/images/earth.jpg';
 import moonUrl from './assets/images/moon.jpg';
@@ -9,18 +9,13 @@ import normalUrl from './assets/images/normal.jpg';
 
 //Builds canvas
 const scene = new THREE.Scene();
-const scene1 = new THREE.Scene();
 const aspectRatio = window.innerWidth / window.innerHeight;
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
-// Use this if you want to enable a grid overlay that will help with setting camera angles and scene layout.
-//const grid = new THREE.GridHelper(5000, 500);
-//scene.add(grid);
-
 //Camera
-const camera = new THREE.PerspectiveCamera(80, aspectRatio, .001, 30000);
+const camera = new THREE.PerspectiveCamera(80, aspectRatio, 0.001, 30000);
 const cameraPositions = {
   x: -2000,
   y: -2000,
@@ -32,11 +27,11 @@ function moveCamera() {
   const x = Object.values(cameraPositions)[0] + document.body.getBoundingClientRect().top;
   const y = Object.values(cameraPositions)[1] + document.body.getBoundingClientRect().top;
   const z = Object.values(cameraPositions)[2] + document.body.getBoundingClientRect().top;
-  //Move camera postion
-    camera.position.x = x * -0.301;
-    camera.position.y = y * -0.2;
-    camera.position.z = z * 0.301;
- };
+  //Move camera position
+  camera.position.x = x * -0.301;
+  camera.position.y = y * -0.2;
+  camera.position.z = z * 0.301;
+}
 document.body.onscroll = moveCamera;
 moveCamera();
 
@@ -45,13 +40,14 @@ const pointLight = new THREE.PointLight(0xffddbf, 4, 4000, 2);
 pointLight.position.set(200, 0, 500);
 
 //Object textures
-const space = new THREE.TextureLoader().load(starsUrl);
 const sunTexture = new THREE.TextureLoader().load(sunUrl);
 const earthTexture = new THREE.TextureLoader().load(earthUrl);
 const moonTexture = new THREE.TextureLoader().load(moonUrl);
 const normalTexture = new THREE.TextureLoader().load(normalUrl);
 
 //Background
+import spaceUrl from './assets/images/space.jpg';
+const space = new THREE.TextureLoader().load(spaceUrl);
 scene.background = space;
 
 //Build Sun
@@ -80,7 +76,7 @@ const moon = new THREE.Mesh(
   new THREE.SphereGeometry(15, 28, 32),
   new THREE.MeshStandardMaterial({
     map: moonTexture,
-    normalMap: normalTexture
+    normalMap: normalTexture,
   })
 );
 const moonObj = new THREE.Object3D();
@@ -96,34 +92,48 @@ function addStar() {
   });
   const star = new THREE.Mesh(geometry, material);
   //FloatSpread (how large the star field will be)
-  const [x, y, z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(2600));
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(4000));
   star.position.set(x, y, z);
   scene.add(star);
-};
-Array(600).fill().forEach(addStar);
+}
+Array(800).fill().forEach(addStar);
 
 //Add the objects to scene
 scene.add(pointLight, earthObj, sun);
 
-
 const controls = new OrbitControls(camera, renderer.domElement);
 
-//animates scene
+// Function to handle window resize
+function onWindowResize() {
+  // Update camera aspect ratio to match the new window aspect ratio
+  const newAspectRatio = window.innerWidth / window.innerHeight;
+  camera.aspect = newAspectRatio;
+  camera.updateProjectionMatrix();
+
+  // Update renderer size
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+onWindowResize();
+// Add event listener for window resize
+window.addEventListener('resize', onWindowResize);
+
+// Function to animate the scene
 function animate() {
   requestAnimationFrame(animate);
-  
-  sun.rotation.y += -0.01;
 
-  moon.rotation.y += 0.00001; //Moon spin
-  moonObj.rotation.y += 0.04; //Moon speed around Earth
+  sun.rotation.y += -0.003;
 
-  earth.rotation.y += 0.03; //Earth spin ALSO CONTROLS moonObj!!!
-  earthObj.rotation.y += 0.008; //Earth speed around Sun
+  moon.rotation.y += -0.01; //Moon spin
+  moonObj.rotation.y += 0.02; //Moon speed around Earth
+
+  earth.rotation.y += -0.008; //Earth spin ALSO CONTROLS moonObj!!!
+  earthObj.rotation.y += 0.002; //Earth speed around Sun
 
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  controls.update();
   renderer.render(scene, camera);
-};
+}
 
+// Start the animation loop
 animate();
